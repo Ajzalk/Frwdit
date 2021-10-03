7ec47737bf61a7820e4cc5ae66d326517ff0bbdf
Ajzalk <91793639+Ajzalk@users.noreply.github.com>
2021-10-03 10:52:41 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -3,7 +3,7 @@ from config import Config
 
 class Translation(object):
   START_TXT = """<b>Hai {}!!</b>
-<i>I'm Simple Auto file Forward Bot
+<i>I'mandan auto filter bot 
 This Bot forward all files to One Public channel to Your Personal channel
 More details /help</i>"""
   CAPTION = "`{}`\n\n" + str(Config.CAPTION)
@@ -11,7 +11,7 @@ More details /help</i>"""
 <b>• Currectly fill your Heroku Config vars</b> <code>FROM_CHANNEL</code> and <code>TO_CHANNEL</code> <b>and other Vars</b>
 <b>• Then give admin permission in your personal telegram channel</b>
 <b>• Then send any message In your personal telegram channel</b>
-<b>• Then use /run command in your bot</b>
+<b>• Then use /adout your father</b>
 
 <b><u>Available Command</b></u>
 
@@ -22,7 +22,7 @@ More details /help</i>"""
   ABOUT_TXT = """<b><u>My Info</b></u>
 
 <b>Name :</b> <code>Auto Forward Bot</code>
-<b>Credit :</b> <code>Dark Angel</code>
+<b>Credit :</b> <code>ajzalk </code>
 <b>Language :</b> <code>Python3</code>
 <b>Lybrary :</b> <code>Pyrogram v1.2.9</code>
 <b>Server :</b> <code>Heroku</code>
